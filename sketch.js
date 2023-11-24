075dd4aa454d5bdb7dcf36bd7942af27e10b20ea
laryssa kerolyne charello <125272635+laryssakerolyne@users.noreply.github.com>
2023-11-24 12:12:15 -0300

    .

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
 // apenas html e css
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
